df34044a1e608b9895fe9ef7dd7c0ed3a3250b23
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-02-10 04:57:51 +0200

    feat: add User entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ name: 'password_hash' })
+  passwordHash: string;
+
+  @CreateDateColumn({ type: 'timestamptz', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', name: 'updated_at' })
+  updatedAt: Date;
+}
