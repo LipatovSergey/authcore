d4e42ea4d0deef21eb38608be756501e20013475
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-02-24 00:58:03 +0200

    feat(test): add login tests

---
+++ test/login.e2e-spec.ts
@@ -0,0 +1,56 @@
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import type { App } from 'supertest/types';
+import { DataSource } from 'typeorm';
+import { createTestApp } from './helpers/test-app.helper';
+
+describe('/auth/login (POST)', () => {
+  let app: INestApplication<App>;
+  let dataSource: DataSource;
+  let httpServer: App;
+
+  beforeAll(async () => {
+    ({ app, dataSource, httpServer } = await createTestApp());
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    await dataSource.query('TRUNCATE TABLE users RESTART IDENTITY CASCADE');
+    await request(httpServer).post('/auth/register').send({
+      email: 'tester@gmail.com',
+      password: 'some spaced text',
+    });
+  });
+
+  it('returns 200', async () => {
+    const res = await request(httpServer).post('/auth/login').send({
+      email: 'tester@gmail.com',
+      password: 'some spaced text',
+    });
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('returns 401 when user does not exist', async () => {
+    const res = await request(httpServer).post('/auth/login').send({
+      email: 'no-tester@gmail.com',
+      password: 'some spaced text',
+    });
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.message).toBe('Invalid credentials');
+  });
+
+  it('returns 401 when password is invalid', async () => {
+    const res = await request(httpServer).post('/auth/login').send({
+      email: 'tester@gmail.com',
+      password: 'wrong password',
+    });
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.message).toBe('Invalid credentials');
+  });
+});
