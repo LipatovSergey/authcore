ccbe5705ef3e70078253481946e2fbd9f2c6f202
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-04-04 11:14:48 +0300

    test(helpers): add env override support for test app

--- test/helpers/test-app.helper.ts
+++ test/helpers/test-app.helper.ts
@@ -6,7 +6,29 @@ import { MailService } from '../../src/auth/providers/mail.service';
 import type { App } from 'supertest/types';
 import { mailServiceMock } from '../mocks/mail-service.mock';
 
+export async function createTestApp(
+  envOverrides?: Record<string, string | undefined>,
+) {
+  const applyEnv = (envValues: Record<string, string | undefined>) => {
+    Object.entries(envValues).forEach(([key, value]) => {
+      if (value === undefined) {
+        delete process.env[key];
+      } else {
+        process.env[key] = value;
+      }
+    });
   };
+
+  const previousEnv: Record<string, string | undefined> = {};
+  if (envOverrides) {
+    // save default values as they set in env
+    Object.keys(envOverrides).forEach((key) => {
+      previousEnv[key] = process.env[key];
+    });
+
+    // override values in process.env
+    applyEnv(envOverrides);
+  }
 
   const moduleFixture: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
@@ -27,9 +49,14 @@ import { mailServiceMock } from '../mocks/mail-service.mock';
   const dataSource = app.get(DataSource);
   const httpServer = app.getHttpServer();
 
+  const restoreEnv = () => {
+    applyEnv(previousEnv);
+  };
+
   return {
     app,
     dataSource,
     httpServer,
+    restoreEnv,
   };
 }
