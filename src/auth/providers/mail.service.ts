e319da70baa5eb7e794e30cdb6f7016133edf9b8
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-04-23 03:38:01 +0300

    feat(auth): add method to send password reset links

--- src/auth/providers/mail.service.ts
+++ src/auth/providers/mail.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 
 export interface MailServiceContract {
   sendEmailVerification(email: string, verificationLink: string): Promise<void>;
+  sendPasswordReset(email: string, verificationLink: string): Promise<void>;
 }
 
 @Injectable()
@@ -14,5 +15,11 @@ export class MailService implements MailServiceContract {
     this.logger.log(
       `Email verification link for ${email}: ${verificationLink}`,
     );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async sendPasswordReset(email: string, passwordResetLink: string) {
+    console.log(`Password reset link for ${email}: ${passwordResetLink}`);
+    this.logger.log(`Password reset link for ${email}: ${passwordResetLink}`);
   }
 }
