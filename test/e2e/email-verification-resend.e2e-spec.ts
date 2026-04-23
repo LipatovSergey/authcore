c23bb62fdf654ae64e6de09c98058aeec360ba63
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-04-23 22:38:08 +0300

    test(e2e): add test case

--- test/e2e/email-verification-resend.e2e-spec.ts
+++ test/e2e/email-verification-resend.e2e-spec.ts
@@ -18,6 +18,7 @@ describe('/auth/email-verification/resend (POST)', () => {
   let userRepository: Repository<User>;
   let mailServiceMock: MailServiceMock;
   const userEmail = 'tester@gmail.com';
+  const endpoint = '/auth/email-verification/resend';
 
   beforeAll(async () => {
     ({ app, dataSource, httpServer, mailServiceMock } = await createTestApp());
@@ -46,7 +47,7 @@ describe('/auth/email-verification/resend (POST)', () => {
       email: userEmail,
     });
     const res = await request(httpServer)
-      .post('/auth/email-verification/resend')
+      .post(endpoint)
       .send({ email: userEmail });
     const newEmailVerificationUrl =
       getLastEmailVerificationUrl(mailServiceMock);
@@ -72,7 +73,7 @@ describe('/auth/email-verification/resend (POST)', () => {
       `/auth/email-verification?token=${verificationToken}`,
     );
     const res = await request(httpServer)
-      .post('/auth/email-verification/resend')
+      .post(endpoint)
       .send({ email: userEmail });
     const newEmailVerificationUrl =
       getLastEmailVerificationUrl(mailServiceMock);
@@ -83,12 +84,23 @@ describe('/auth/email-verification/resend (POST)', () => {
 
   it('returns 200 and does not send a verification link for non-existent user', async () => {
     const res = await request(httpServer)
-      .post('/auth/email-verification/resend')
+      .post(endpoint)
       .send({ email: 'non-existent-user@test.com' });
     const newEmailVerificationUrl =
       getLastEmailVerificationUrl(mailServiceMock);
     expect(res.statusCode).toBe(200);
     expect(res.body.message).toBe('ok');
     expect(firstEmailVerificatonUrl.href).toBe(newEmailVerificationUrl.href);
+  });
+
+  it('returns 200 with ok message, even if mail sending failed', async () => {
+    mailServiceMock.sendEmailVerification.mockRejectedValueOnce(
+      new Error('Email verification resend failed'),
+    );
+    const res = await request(httpServer)
+      .post(endpoint)
+      .send({ email: userEmail });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe('ok');
   });
 });
