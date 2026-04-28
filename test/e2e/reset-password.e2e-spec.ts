a84d624298cd05cb4c9884e26b7b20a88d54a208
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-04-28 08:24:25 +0300

    test(e2e): add reset-password tests

---
+++ test/e2e/reset-password.e2e-spec.ts
@@ -0,0 +1,70 @@
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import type { App } from 'supertest/types';
+import { DataSource } from 'typeorm';
+import { createTestApp } from '../helpers/test-app.helper';
+import {
+  getLastResetPasswordUrl,
+  type MailServiceMock,
+} from '../mocks/mail-service.mock';
+
+describe('/auth/reset-password (POST)', () => {
+  let app: INestApplication<App>;
+  let dataSource: DataSource;
+  let httpServer: App;
+  let mailServiceMock: MailServiceMock;
+  let passwordResetToken: string;
+  const endpoint = '/auth/reset-password';
+  const userEmail = 'tester@gmail.com';
+
+  beforeAll(async () => {
+    ({ app, dataSource, httpServer, mailServiceMock } = await createTestApp());
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    mailServiceMock.reset();
+    await dataSource.query('TRUNCATE TABLE users RESTART IDENTITY CASCADE');
+    await request(httpServer)
+      .post('/auth/register')
+      .send({
+        email: userEmail,
+        password: 'some spaced text',
+      })
+      .expect(201);
+    await request(httpServer)
+      .post('/auth/forgot-password')
+      .send({ email: userEmail })
+      .expect(200);
+    const url = getLastResetPasswordUrl(mailServiceMock);
+    passwordResetToken = url.searchParams.get('token') as string;
+  });
+
+  it('returns 200 with ok message for valid credentials', async () => {
+    const res = await request(httpServer).post(endpoint).send({
+      token: passwordResetToken,
+      password: 'new valid password',
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe('ok');
+  });
+
+  it('returns 400 for invalid password', async () => {
+    const res = await request(httpServer).post(endpoint).send({
+      token: passwordResetToken,
+      password: '123',
+    });
+    expect(res.statusCode).toBe(400);
+  });
+
+  it('returns 401 for invalid token', async () => {
+    const res = await request(httpServer).post(endpoint).send({
+      token: 'invalid-password-reset-token',
+      password: 'new valid password',
+    });
+    expect(res.statusCode).toBe(401);
+  });
+});
