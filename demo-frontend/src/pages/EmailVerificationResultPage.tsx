2a614481901768e7bc3028b0bfd82d6df2c0d744
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-05-06 08:37:45 +0300

    feat(frontend): add email verification result page

--- demo-frontend/src/pages/EmailVerificationResultPage.tsx
+++ demo-frontend/src/pages/EmailVerificationResultPage.tsx
@@ -1,3 +1,22 @@
+import { useSearchParams } from 'react-router-dom';
+
 export function EmailVerificationResultPage() {
-  return <h1>EmailVerificationResultPage</h1>;
+  const [searchParams] = useSearchParams();
+  const status = searchParams.get('status');
+  function getVerificationMessage(status: string | null) {
+    if (status === 'verified') {
+      return 'Email verified. You can now sign in.';
+    }
+    if (status === 'already_verified') {
+      return 'Email is already verified.';
+    }
+
+    return 'Verification link is invalild or expired.';
+  }
+
+  return (
+    <>
+      <h1>{getVerificationMessage(status)}</h1>
+    </>
+  );
 }
